3d12ced201f170acb39ad0898f769832f2fd907e
Matthew Carlson <matt@mcarlson.xyz>
2021-07-10 01:59:31 +0000

    clean up

--- config.h
+++ config.h
@@ -1,47 +1,53 @@
-/* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
+// dmenu
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+// topbar
+static int topbar = 1;
+
+// centered
+static int centered = 0;
+
+// min width when centered
+static int min_width = 500;
+
+// colored prompt
 static int colorprompt = 1;
 
-/* -fn option overrides fonts[0]; default X11 font or font set */
+// font
 static char font[] = "monospace:size=10";
 static const char *fonts[] = {
 	font,
 	"monospace:size=10",
 };
 
-static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+// prompt
+static char *prompt = NULL;
 
+// colors
 static char normfgcolor[] = "#bbbbbb";
 static char normbgcolor[] = "#222222";
 static char selfgcolor[]  = "#eeeeee";
 static char selbgcolor[]  = "#005577";
+
+// color array
 static char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+	//               fg           bg
 	[SchemeNorm] = { normfgcolor, normbgcolor },
 	[SchemeSel]  = { selfgcolor,  selbgcolor  },
 	[SchemeOut]  = { "#000000",   "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
+// lines for vertical dmenu
+static unsigned int lines = 0;
+
+// chars not considered part of word
 static const char worddelimiters[] = " ";
 
-/* Size of the window border */
+// border width
 static unsigned int border_width = 5;
 
-/*
- * Xresources preferences to load at startup
- */
+// xresources
 ResourcePref resources[] = {
-	{ "font",         STRING,  &font },
+	{ "font",         STRING,  &font         },
 	{ "normfgcolor",  STRING,  &normfgcolor  },
 	{ "normbgcolor",  STRING,  &normbgcolor  },
 	{ "selfgcolor",   STRING,  &selfgcolor   },
